15f8c2064e75648ebade844a5a497646210f7cdd
GotNukesGit <gsamelon@gmail.com>
2026-07-15 18:40:51 -0700

    Add GTNH build scaffold, mod entry point, dependencies

---
+++ src/main/java/com/zircaloylabs/powermonitor/PowerMonitorMod.java
@@ -0,0 +1,34 @@
+package com.zircaloylabs.powermonitor;
+
+import cpw.mods.fml.common.Mod;
+import cpw.mods.fml.common.event.FMLInitializationEvent;
+import cpw.mods.fml.common.event.FMLPreInitializationEvent;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+/**
+ * Mod entry point. This class was NOT present before this session -- without
+ * an @Mod-annotated class, Forge has nothing to load, regardless of how
+ * correct the cover/item/registration logic underneath it is. Registration
+ * runs in preInit, matching the comment on ModPowerMonitorRegistration.
+ */
+@Mod(modid = PowerMonitorMod.MODID, version = PowerMonitorMod.VERSION,
+        name = "Power Network Monitor", acceptedMinecraftVersions = "[1.7.10]",
+        dependencies = "required-after:gregtech")
+public class PowerMonitorMod {
+
+    public static final String MODID = "powermonitor";
+    public static final String VERSION = "1.0.0";
+    public static final Logger LOG = LogManager.getLogger(MODID);
+
+    @Mod.EventHandler
+    public void preInit(FMLPreInitializationEvent event) {
+        LOG.info("Power Network Monitor preInit -- registering item, covers, recipes");
+        ModPowerMonitorRegistration.registerAll();
+    }
+
+    @Mod.EventHandler
+    public void init(FMLInitializationEvent event) {
+        // Nothing yet -- registration is fully handled in preInit.
+    }
+}
