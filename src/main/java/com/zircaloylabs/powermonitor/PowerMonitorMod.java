798910aab6a3989ac9055ded109d4d00241fc677
GotNukes <gsamelon@gmail.com>
2026-07-16 09:11:19 -0700

    Update PowerMonitorMod.java

--- src/main/java/com/zircaloylabs/powermonitor/PowerMonitorMod.java
+++ src/main/java/com/zircaloylabs/powermonitor/PowerMonitorMod.java
@@ -1,11 +1,10 @@
 package com.zircaloylabs.powermonitor;
-
 import cpw.mods.fml.common.Mod;
 import cpw.mods.fml.common.event.FMLInitializationEvent;
 import cpw.mods.fml.common.event.FMLPreInitializationEvent;
+import cpw.mods.fml.relauncher.Side;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
-
 /**
  * Mod entry point. This class was NOT present before this session -- without
  * an @Mod-annotated class, Forge has nothing to load, regardless of how
@@ -16,17 +15,17 @@ import org.apache.logging.log4j.Logger;
         name = "Power Network Monitor", acceptedMinecraftVersions = "[1.7.10]",
         dependencies = "required-after:gregtech")
 public class PowerMonitorMod {
-
     public static final String MODID = "powermonitor";
     public static final String VERSION = "1.0.0";
     public static final Logger LOG = LogManager.getLogger(MODID);
-
     @Mod.EventHandler
     public void preInit(FMLPreInitializationEvent event) {
         LOG.info("Power Network Monitor preInit -- registering item, covers, recipes");
         ModPowerMonitorRegistration.registerAll();
+        if (event.getSide() == Side.CLIENT) {
+            PowerMonitorIcons.registerSelf();
+        }
     }
-
     @Mod.EventHandler
     public void init(FMLInitializationEvent event) {
         // Nothing yet -- registration is fully handled in preInit.
